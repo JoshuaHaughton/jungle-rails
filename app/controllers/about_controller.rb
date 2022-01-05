0544f9711dbca22baf53f7dd17d5af52f9bf77d8
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-05 21:41:31 +0000

    /about#index renders an About Us page, which is now acessible in the top nav

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,10 @@
 class AboutController < ApplicationController
 
   def index
-    
+    @product_list_length = Product.all.length
+    @random_product_length = 1 + rand(@product_list_length)
+    @random_product = Product.all[@random_product_length]
+    puts Product.all[0].methods
   end
 
 end
