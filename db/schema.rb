15cfb63e80dbdadd39fded01f2e7d112b452bdc3
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-06 22:48:26 +0000

    Routes and views for admin sales created, Sales dashboard shows a list of all current sales

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20220106222845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.boolean "answer",                      null: false
+    t.boolean "is_correct",  default: false, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +49,12 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.string   "email"
   end
 
+  create_table "previous_attempts", force: :cascade do |t|
+    t.integer "quiz_id"
+    t.integer "user_id"
+    t.integer "score"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -56,7 +68,39 @@ ActiveRecord::Schema.define(version: 20160625062916) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
+  create_table "questions", force: :cascade do |t|
+    t.integer "quiz_id"
+    t.text    "question", null: false
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.integer "owner_id"
+    t.string  "title",       limit: 255,                null: false
+    t.text    "description",                            null: false
+    t.boolean "public",                  default: true
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "percent_off"
+    t.date     "starts_on"
+    t.date     "ends_on"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username",        limit: 255, null: false
+    t.string "email",           limit: 255, null: false
+    t.string "password_digest", limit: 255, null: false
+  end
+
+  add_foreign_key "answers", "questions", name: "answers_question_id_fkey", on_delete: :cascade
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "previous_attempts", "quizzes", name: "previous_attempts_quiz_id_fkey", on_delete: :cascade
+  add_foreign_key "previous_attempts", "users", name: "previous_attempts_user_id_fkey", on_delete: :cascade
   add_foreign_key "products", "categories"
+  add_foreign_key "questions", "quizzes", name: "questions_quiz_id_fkey", on_delete: :cascade
+  add_foreign_key "quizzes", "users", column: "owner_id", name: "quizzes_owner_id_fkey", on_delete: :cascade
 end
