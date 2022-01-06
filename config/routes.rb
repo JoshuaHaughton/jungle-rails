3f81371b3fe1434df1c4f5745b5c6d76e43c156e
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-06 20:25:03 +0000

    Users controller and user registration page created

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  	
+	get 'users/new' => 'users#new', as: :new_user
+	post 'users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
